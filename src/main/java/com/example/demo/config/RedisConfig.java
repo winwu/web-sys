b334051c34b945d1feeecc0f433a610fd5117b4d
winwu <look_me08@hotmail.com>
2021-06-12 13:47:57 +0800

    clean RedisConfig.java

--- src/main/java/com/example/demo/config/RedisConfig.java
+++ src/main/java/com/example/demo/config/RedisConfig.java
@@ -1,18 +1,11 @@
 package com.example.demo.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
@@ -33,9 +26,6 @@ public class RedisConfig extends CachingConfigurerSupport {
 
     @Bean
     public RedisCacheManager redisCacheManager(RedisTemplate redisTemplate) {
-        // https://stackoverflow.com/questions/55088211/configure-a-new-serializer-for-spring-boot-redis-cache-config
-
-        // the reason to set up this config:
         // when using @Cacheable which will store value in bytes format in redis,
         // but we need to change it, store JSON format instead of bytes.
         RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisTemplate.getConnectionFactory());
@@ -45,63 +35,6 @@ public class RedisConfig extends CachingConfigurerSupport {
         return new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
     }
 
-    // default serializer is JdkSerializationRedisSerializer
-//    @Bean
-//    @SuppressWarnings({ "rawtypes", "unchecked" })
-//    public RedisTemplate<String, Object> redisTemplate() {
-//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
-//        // 注入数据源
-//        redisTemplate.setConnectionFactory(redisConnectionFactory);
-//        // 使用Jackson2JsonRedisSerialize 替换默认序列化
-//        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-//        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-//
-//        // key-value结构序列化数据结构
-//        redisTemplate.setKeySerializer(stringRedisSerializer);
-//        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
-//        // hash数据结构序列化方式,必须这样否则存hash 就是基于jdk序列化的
-//        redisTemplate.setHashKeySerializer(stringRedisSerializer);
-//        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
-//        // 启用默认序列化方式
-//        redisTemplate.setEnableDefaultSerializer(true);
-//        redisTemplate.setDefaultSerializer(jackson2JsonRedisSerializer);
-//
-//        redisTemplate.afterPropertiesSet();
-//        return redisTemplate;
-//    }
-
-//
-
-
-
-//    @SuppressWarnings({"rawtypes", "unchecked"})
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-//        // redis 沒有支援 POJO 的格式 所以要自己序列化
-//        RedisTemplate<String, Object> template = new RedisTemplate();
-//        template.setConnectionFactory(connectionFactory);
-//
-//        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-//
-//        // 使用 Jackson2JsonRedisSerializer 做為 serializer
-//        // 要序列化的部分,field 的 get/set; 修飾字的範圍包含 private/public
-//        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-//        ObjectMapper om = new ObjectMapper();
-//        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-//
-//        // 如果沒有這一行 存到 redis 的資料是沒有型別的
-//        // om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
-//        jackson2JsonRedisSerializer.setObjectMapper(om);
-//
-//        // Set the serialization rules of value and key
-//        template.setKeySerializer(stringRedisSerializer);
-//        template.setValueSerializer(jackson2JsonRedisSerializer);
-//        template.setHashKeySerializer(stringRedisSerializer);
-//        template.setHashValueSerializer(jackson2JsonRedisSerializer);
-//        template.setDefaultSerializer(jackson2JsonRedisSerializer);
-//        template.afterPropertiesSet();
-//        return template;
-//    }
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate();
@@ -111,12 +44,6 @@ public class RedisConfig extends CachingConfigurerSupport {
         Jackson2JsonRedisSerializer<Object> defaultSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
         GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
 
-//        ObjectMapper om = new ObjectMapper();
-//        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-//        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
-//        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-//        defaultSerializer.setObjectMapper(om);
-
         redisTemplate.setKeySerializer(stringRedisSerializer);
         redisTemplate.setValueSerializer(serializer);
         redisTemplate.setHashKeySerializer(stringRedisSerializer);
@@ -124,5 +51,4 @@ public class RedisConfig extends CachingConfigurerSupport {
         redisTemplate.setDefaultSerializer(defaultSerializer);
         return redisTemplate;
     }
-
 }
