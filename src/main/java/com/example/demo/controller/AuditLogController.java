6e169e792576d989464cb1ebb1565a87d5404b56
winwu <look_me08@hotmail.com>
2021-07-11 15:41:56 +0800

    add search api in AuditLogController

--- src/main/java/com/example/demo/controller/AuditLogController.java
+++ src/main/java/com/example/demo/controller/AuditLogController.java
@@ -3,15 +3,21 @@ package com.example.demo.controller;
 import com.example.demo.entity.AuditLog;
 import com.example.demo.repository.AuditLogRepository;
 import com.example.demo.service.impl.AuditLogServiceImpl;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 
@@ -46,6 +52,29 @@ public class AuditLogController {
     public ResponseEntity getAuditByUserName(@PathVariable("name") String name) {
         List<AuditLog> logs = auditLogRepository.findByUserName(name);
         return new ResponseEntity<>(logs, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public ResponseEntity getSearchResult(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "limit", defaultValue = "10") Integer limit,
+            @RequestParam(value = "query", defaultValue = "") String query) {
+        NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+
+        // @TODO filter by userName
+        nativeSearchQueryBuilder.withQuery(QueryBuilders.multiMatchQuery(query, "oldContent", "newContent", "eventType", "message"));
+        nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort("createdAt").order(SortOrder.DESC));
+        nativeSearchQueryBuilder.withPageable(PageRequest.of(page, limit));
+        Page<AuditLog> logs = auditLogRepository.search(nativeSearchQueryBuilder.build());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("code", "SUCCESS");
+        response.put("data", logs.getContent());
+        response.put("currentPage", logs.getNumber());
+        response.put("totalRecords", logs.getTotalElements());
+        response.put("totalPages", logs.getTotalPages());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @RequestMapping(value = "test", method = RequestMethod.GET)
